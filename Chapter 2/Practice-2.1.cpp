6ba2a9808f3b3bfebc697526ef0af621bea0d6c2
L-ge <527773230@qq.com>
2017-05-09 23:33:33 +0800

    Add files via upload

---
+++ Chapter 2/Practice-2.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "My name is L-ge." << endl;
+    cout << "My address is in China." << endl;
+	return 0;
+}
---
+++ Chapter 2/Practice-2.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Please input a number: ";
+    int distance;
+    cin >> distance;
+    cout << distance << " longs is "
+                << 220 * distance << " keys." << endl;
+	return 0;
+}
