8d753bda22b29d87eb5f995ad874b29885b6cfe0
L-ge <527773230@qq.com>
2017-05-10 07:59:49 +0800

    Add files via upload

---
+++ Chapter 2/Practice-2.5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+float trans(float);
+
+int main()
+{
+    using namespace std;
+    cout << "Please enter a Celsius value: ";
+    float celsius;
+    cin >> celsius;
+    float fahrenheit = trans(celsius);
+    cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit." << endl;
+	return 0;
+}
+
+float trans(float celsius)
+{
+    return 1.8 * celsius + 32.0;
+}
