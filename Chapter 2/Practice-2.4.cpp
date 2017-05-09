d78f44d57a2977cbaa9c0dc6fb31f5d374b15b4a
L-ge <527773230@qq.com>
2017-05-09 23:36:43 +0800

    Create Practice-2.4.cpp

---
+++ Chapter 2/Practice-2.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "Enter your age: ";
+    int age;
+    cin >> age;
+    cout << "Your age has " << age * 12 << " months." << endl;
+	return 0;
+}
