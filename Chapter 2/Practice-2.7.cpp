895762755751210b0bf32871919d1d999babbdd8
L-ge <527773230@qq.com>
2017-05-10 08:00:45 +0800

    Add files via upload

---
+++ Chapter 2/Practice-2.7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+void showtime(int, int);
+
+int main()
+{
+    using namespace std;
+    int hour, minute;
+    cout << "Enter the number of hours: ";
+    cin >> hour;
+    cout << "Enter the number of minutes: ";
+    cin >> minute;
+    showtime(hour, minute);
+	return 0;
+}
+
+void showtime(int hour, int minute)
+{
+    using namespace std;
+    cout << "Time: " << hour << ":" << minute << endl;
+}
