b4e13e7fb1deeca7d3a3aaab312a48719513cdac
L-ge <527773230@qq.com>
2017-05-10 08:00:25 +0800

    Add files via upload

---
+++ Chapter 2/Practice-2.6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+double trans(double);
+
+int main()
+{
+    using namespace std;
+    cout << "Enyer the number of light years: ";
+    double lightyear;
+    cin >> lightyear;
+    cout << lightyear << " light years = " << trans(lightyear) << " astronomical units." << endl;
+	return 0;
+}
+
+double trans(double lightyear)
+{
+    return 63240 * lightyear;
+}
