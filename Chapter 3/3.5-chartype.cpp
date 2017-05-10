03c67d3d35c37fe4e723aacc3fdaaf78f2946cea
L-ge <527773230@qq.com>
2017-05-10 19:54:13 +0800

    Add files via upload

---
+++ Chapter 3/3.5-chartype.cpp
@@ -0,0 +1,13 @@
+// chartype.cpp -- the char type
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch;
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " charachter." << endl;
+	return 0;
+}
