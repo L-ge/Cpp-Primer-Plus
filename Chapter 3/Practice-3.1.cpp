1f58cbaf77efcbfedd2c12bcd1b96ad6e5f7b8d1
L-ge <527773230@qq.com>
2017-05-14 11:45:15 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int height;
+    cout << "Enter your height(inch): __\b\b";
+    cin >> height;
+    const int Inch_per_foot = 12;
+    cout << "Your height is : " << height / Inch_per_foot << " feet " << height % Inch_per_foot << " inch." << endl;
+	return 0;
+}
