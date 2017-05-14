50bb3bd8cc0aa2b11b8fdec41a93524af25e9b21
L-ge <527773230@qq.com>
2017-05-14 11:46:43 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    float degree, minute, second;
+    cout << "Enter a latitude in degree, minutes, and seconds: " << endl;
+    cout << "First, enter the degrees: ";
+    cin >> degree;
+    cout << "Next, enter the munutes of arc: ";
+    cin >> minute;
+    cout << "Finally, enter the seconds of arc: ";
+    cin >> second;
+
+    const int Minute_per_degree = 60;
+    const int Second_per_minute = 60;
+    cout << degree << " degrees, " << minute << " minutes, " << second << " seconds = "
+         << degree + minute / Minute_per_degree + second / Second_per_minute / Second_per_minute
+         << " degrees" << endl;
+	return 0;
+}
