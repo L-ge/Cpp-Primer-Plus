53c749513a5699835b566485ab3e27f9eb1fe4f0
L-ge <527773230@qq.com>
2017-05-14 11:46:15 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int foot, inch, weight;
+    cout << "Enter the foot of your height: ";
+    cin >> foot;
+    cout << "Enter the inch of your height: ";
+    cin >> inch;
+    cout << "Enter your weight: ";
+    cin >> weight;
+
+    const int Inch_per_foot = 12;
+    const float Pounds_per_kilogram = 2.2;
+    const float Meter_per_inch = 0.0254;
+    cout << "Your BMI is "
+         << (weight / Pounds_per_kilogram) / ((foot * Inch_per_foot + inch) * Meter_per_inch * (foot * Inch_per_foot + inch) * Meter_per_inch)
+         << "." << endl;
+	return 0;
+}
