42efa4d3ca65ea2858e79bc13b3707bee73530f5
L-ge <527773230@qq.com>
2017-05-15 20:33:20 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    long long sum, t;
+    cout << "Enter the world's population: ";
+    cin >> sum;
+    cout << "Enter the population of the US: ";
+    cin >> t;
+    cout << "The population of the US is " << float(t) / float(sum) * 100 << "% of the world population." << endl;
+	return 0;
+}
