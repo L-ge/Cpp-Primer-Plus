992ed6c5834ca31fd2aa6f0f4e954918212f97bf
L-ge <527773230@qq.com>
2017-05-15 20:32:17 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    float distance, gasoline;
+    cout << "Enter the distance: ";
+    cin >> distance;
+    cout << "Enter the gasoline: ";
+    cin >> gasoline;
+    cout << "Your car consume one gallon gasoline can run " << distance / gasoline << " miles.\n";
+	return 0;
+}
