3ff85ec0153b193bcbdf194a320a1fc5ce258c69
L-ge <527773230@qq.com>
2017-05-15 20:30:24 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    long long second;
+    cout << "Enter the number of seconds: ";
+    cin >> second;
+
+    const int Hour_per_day = 24;
+    const int Minute_per_hour = 60;
+    const int Second_per_minute = 60;
+    cout << second << " seconds = " << second / (Hour_per_day * Minute_per_hour * Second_per_minute)
+         << " days, " << (second % (Hour_per_day * Minute_per_hour * Second_per_minute)) / (Minute_per_hour * Second_per_minute)
+         << " hours, " << (second % (Minute_per_hour * Second_per_minute)) /  Second_per_minute
+         << " minutes, " << second % Second_per_minute
+         << " seconds" << endl;
+	return 0;
+}
