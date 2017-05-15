150b1d5fd482298889797bafda00ac0dd168b448
L-ge <527773230@qq.com>
2017-05-15 20:35:18 +0800

    Add files via upload

---
+++ Chapter 3/Practice-3.7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    float consume;
+    cout << "Enter your car consume gasline per 100 kilometer: ";
+    cin >> consume;
+    cout << "Your car can run " << int (62.14 / (consume / 3.875)) << "(mpg) miles per gallon gasoline.\n";
+	return 0;
+}
