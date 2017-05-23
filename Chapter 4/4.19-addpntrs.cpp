4c99b1ee913a4ff862dbdf6652a310e086c2aa5f
L-ge <527773230@qq.com>
2017-05-23 15:48:09 +0800

    Add files via upload

---
+++ Chapter 4/4.19-addpntrs.cpp
@@ -0,0 +1,32 @@
+// addpntrs.cpp -- pointer addition
+#include <iostream>
+int main()
+{
+    using namespace std;
+    double wages[3] = {10000.0, 20000.0, 30000.0};
+    short stacks[3] = {3, 2, 1};
+
+  // Here are two ways to get the address of an array
+    double * pw = wages;      // name if an array = address
+    short * ps = &stacks[0];  // or use address operator
+  // with array element
+    cout << "pw = " << pw << ", *pw = " << *pw << endl;
+    pw = pw + 1;
+    cout << "add 1 to the pw pointer:\n";
+    cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << endl;
+    ps = ps + 1;
+    cout << "add 1 to the ps pointer:\n";
+    cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+
+    cout << "access two elements with array notation\n";
+    cout << "stacks[0] = " << stacks[0]
+         << ", stacks[1] = " << stacks[1] << endl;
+    cout << "access two elements with pointer notation\n";
+    cout << "*stacks = " << *stacks
+         << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+
+    cout << sizeof(wages) << " = size of wage array\n";   // 3 X 8 = 24字节
+    cout << sizeof(pw) << " = size of pw pointer\n";      // 4是指针的长度，与double类型是无关的。
+	return 0;
+}
