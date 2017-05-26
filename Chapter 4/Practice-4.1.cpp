723f3e5de91d75a5b776f1708f269c668a21fc4f
L-ge <527773230@qq.com>
2017-05-26 15:26:59 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char firstname[20], lastname[20];
+    char grade;
+    int age;
+    cout << "What is your first name? ";
+    cin.get(firstname,20).get();
+    cout << "What is your last name? ";
+    cin.get(lastname, 20).get();
+    cout << "What letter grade do you deserve? ";
+    cin >> grade;
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "Name: " << lastname << ", " << firstname << endl;
+    cout << "Grade: " << char(grade + 1) << endl;
+    cout << "Age: " << age << endl;
+	return 0;
+}
