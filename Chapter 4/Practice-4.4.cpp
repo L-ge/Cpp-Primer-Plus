9e0ed359f88591ff96e65eb66a3d1139581d4691
L-ge <527773230@qq.com>
2017-05-29 07:51:22 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    using namespace std;
+    string firstname, lastname, name;
+    cout << "Enter your first name: ";
+    getline(cin, firstname);
+    cout << "Enter your last name: ";
+    getline(cin, lastname);
+    lastname += ", ";
+    name += lastname;
+    name += firstname;
+    cout << "Here's the information in a single string: " << name << endl;
+	return 0;
+}
