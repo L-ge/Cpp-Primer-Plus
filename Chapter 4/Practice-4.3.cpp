777038eeef96ec7c8fde57a7fd67996c1fb774c3
L-ge <527773230@qq.com>
2017-05-29 07:51:05 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+int main()
+{
+    using namespace std;
+    char firstname[20], lastname[20], name[40];
+    cout << "Enter your first name: ";
+    cin.get(firstname,20).get();
+    cout << "Enter your last name: ";
+    cin.get(lastname,20).get();
+    strcat(lastname, ", ");
+    strcpy(name, lastname);
+    strcat(name, firstname);
+    cout << "Here's the information in a single string: " << name << endl;
+	return 0;
+}
