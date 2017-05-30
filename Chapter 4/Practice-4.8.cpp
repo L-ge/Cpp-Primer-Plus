bf8e658ac9ae70412eb1d394339dc861c496ef06
L-ge <527773230@qq.com>
2017-05-30 20:49:56 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+struct William
+{
+    std::string name;
+    float length;
+    float weight;
+};
+int main()
+{
+    using namespace std;
+    William * pw = new William;
+
+    cout << "Enter the length: ";
+    cin >> pw->length;
+    cin.get();
+    cout << "Enter the name: ";
+    getline(cin, pw->name);
+    cout << "Enter the weight: ";
+    cin >> pw->weight;
+    cout << "\nName: " << pw->name
+         << "\nLength: " << pw->length
+         << "\nWeight: " << pw->weight
+         << endl;
+    delete pw;
+	return 0;
+}
