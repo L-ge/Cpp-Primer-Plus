8bb06af529a6e06b17f0b58b83b5f5fa32ef49fd
L-ge <527773230@qq.com>
2017-05-30 20:45:45 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+struct CandyBar
+{
+    std::string brand;
+    float weight;
+    int power;
+};
+int main()
+{
+    CandyBar snack = {"Mocha", 2.3, 350};
+    std::cout << "Brand: " << snack.brand
+         << "\nWeight: " << snack.weight
+         << "\npower: " << snack.power
+         << std::endl;
+	return 0;
+}
