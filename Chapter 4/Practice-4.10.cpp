d11962f39992028eef9494b093c1b16644fa0c7f
L-ge <527773230@qq.com>
2017-05-31 22:11:59 +0800

    Add files via upload

---
+++ Chapter 4/Practice-4.10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+int main()
+{
+    using namespace std;
+    array<float, 3> grade;
+    cout << "Enter the first grade: ";
+    cin >> grade[0];
+    cout << "Enter the second grade: ";
+    cin >> grade[1];
+    cout << "Enter the third grade: ";
+    cin >> grade[2];
+
+    cout << "The average of the 3 grades is "
+         << (grade[0] + grade[1] + grade[2]) / 3
+         << endl;
+	return 0;
+}
