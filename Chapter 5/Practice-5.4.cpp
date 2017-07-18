3a66d6cf93157d94eaaa3d56b6e6a6445dc53403
L-ge <527773230@qq.com>
2017-07-18 20:08:07 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    float daphne = 100 + 0.1 * 100;
+    float cleo = 100 + 0.05 * 100;
+    int year =  1;
+    while (cleo <= daphne)
+    {
+        cleo += 0.05 * cleo;
+        daphne = daphne + 0.1 * 100;
+        ++year;
+    }
+    cout << "After " << year << " years, "
+         << "Cleo has $" << cleo
+         << ", Daphne has $" << daphne
+         << ".\n";
+	return 0;
+}
