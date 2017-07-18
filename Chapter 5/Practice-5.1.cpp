566d85324949dc99a980f45e6f30f0e2b20a4210
L-ge <527773230@qq.com>
2017-07-18 19:58:27 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int small, big;
+    cout << "Enter the two integers: ";
+    cin >> small >> big;
+    int sum = 0;
+    for (int i = small; i <= big; ++i)
+        sum += i;
+    cout << "The sum is " << sum;
+	return 0;
+}
