b57e141a24200d1d8bb8539b96c9f7024d5f6998
L-ge <527773230@qq.com>
2017-07-18 20:02:34 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int number, sum = 0;
+    cout << "Enter a number(input 0 to quit): \n";
+    cin >> number;
+    while(number != 0)
+    {
+        sum += number;
+        cout << "The sum is " << sum << " after input " << number <<endl;
+        cout << "Enter another number(input 0 to quit): \n";
+        cin >> number;
+    }
+	return 0;
+}
