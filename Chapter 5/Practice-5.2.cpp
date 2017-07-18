95e906345b53182a06a7b3b999a9e271e997b336
L-ge <527773230@qq.com>
2017-07-18 20:00:06 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+const int ArSize = 101;
+int main()
+{
+    std::array<long double, ArSize> factorials;
+    factorials[1] = factorials[0] = 1LL;
+    for (int i = 2; i < ArSize; i++)
+        factorials[i] = i * factorials[i-1];
+    std::cout << "100! is " << factorials[100] << std::endl;
+	return 0;
+}
