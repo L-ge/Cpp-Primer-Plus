6f7da5a43d7441b60e7395ad536bd1c8aee25c18
L-ge <527773230@qq.com>
2017-07-22 19:02:36 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    string month[12] =
+    {
+       "January",
+       "February",
+       "March",
+       "April",
+       "May",
+       "June",
+       "July",
+       "August",
+       "September",
+       "October",
+       "November",
+       "December"
+    };
+    int sale[12];
+    int sum = 0;
+    cout << "Enter the year's sale: \n";
+    for (int i = 0; i < 12; ++i)
+    {
+        cout << month[i] << " is ";
+        cin >> sale[i];
+        sum += sale[i];
+    }
+    cout << "\nThe total sale is " << sum << endl;
+    for (int i = 0; i < 12; ++i)
+        cout << month[i] << " is " << sale[i] << endl;
+	return 0;
+}
