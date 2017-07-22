ddfd74dcc5d02517d690bbc8ae4fc2f89b8b4f77
L-ge <527773230@qq.com>
2017-07-22 20:06:19 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+int main()
+{
+    using namespace std;
+    cout << "Enter words (to stop, type the word done): \n";
+    char word[30];
+    cin >> word;
+    int count = 0;
+    while (strcmp(word, "done"))
+    {
+        ++count;
+        cin >> word;
+    }
+    cout << "You entered a total of " << count << " words.\n";
+	return 0;
+}
