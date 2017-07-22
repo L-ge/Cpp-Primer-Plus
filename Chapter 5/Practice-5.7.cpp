770a311e0c99e31ea619c74b26f01d931ff3ccb8
L-ge <527773230@qq.com>
2017-07-22 19:04:16 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+struct car
+{
+    std::string producer;
+    int year;
+};
+int main()
+{
+    using namespace std;
+    cout << "How many cars do you wish to catalog? ";
+    int number;
+    cin >> number;
+    car * pc = new car[number];
+    for (int i = 0; i < number; ++i)
+    {
+        cin.get();
+        cout << "Car #" << i + 1 << ":\n";
+        cout << "Please enter the make: ";
+        getline(cin, pc[i].producer);
+        cout << "Please enter the year made: ";
+        cin >> pc[i].year;
+    }
+    cout << "Here is your collection: \n";
+    for (int i = 0; i < number; ++i)
+    {
+       cout << pc[i].year << " " << pc[i].producer << endl;
+    }
+	return 0;
+}
