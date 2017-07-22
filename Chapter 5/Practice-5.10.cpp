7cf0d18c4689f6757aba240545b9264b80b4fcc1
L-ge <527773230@qq.com>
2017-07-22 20:07:38 +0800

    Add files via upload

---
+++ Chapter 5/Practice-5.10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int row;
+    cout << "Enter number of rows: ";
+    cin >> row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = row - i - 1; j > 0; j--)
+            cout << ".";
+        for (int k = 0; k <= i; k++)
+            cout << "*";
+        cout << endl;
+    }
+	return 0;
+}
