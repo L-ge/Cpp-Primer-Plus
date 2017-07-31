6f5ea809f1c244dbcc73168e6fda038551062775
L-ge <527773230@qq.com>
2017-07-31 20:08:14 +0800

    Add files via upload

---
+++ Chapter 6/Practice-6.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    double donation[10];
+    int i = 0;
+    double sum = 0.0;
+    while (i < 10 && cin >> donation[i])
+    {
+        sum += donation[i];
+        ++i;
+    }
+    double ave = sum / i;
+    cout << "Average: " << ave << ", \n";
+    int count = 0;
+    for (int j = 0; j < i; ++j)
+    {
+        if (donation[j] > ave)
+            ++count;
+    }
+    cout << "There has " << count << " numbers bigger than average.\n";
+	return 0;
+}
