ecd94010f8e278bcb1591cf776d7ce9f5634d68f
L-ge <527773230@qq.com>
2017-07-31 20:07:48 +0800

    Add files via upload

---
+++ Chapter 6/Practice-6.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cctype>
+int main()
+{
+    using namespace std;
+    char ch;
+    cin.get(ch);
+    while (ch != '@')
+    {
+        if (islower(ch))
+            cout << char(toupper(ch));
+        else if (isupper(ch))
+            cout << char(tolower(ch));
+        cin.get(ch);
+    }
+	return 0;
+}
