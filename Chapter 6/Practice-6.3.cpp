22c3267e25243d9c5db2c1fc4ea7f4c2a18c3275
L-ge <527773230@qq.com>
2017-08-01 20:05:38 +0800

    Add files via upload

---
+++ Chapter 6/Practice-6.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Please enter one of the following choices: \n"
+         << "c) carnivore            p) pianist\n"
+         << "t) tree                 g) game\n";
+    char choice;
+    cin >> choice;
+    while (choice != 'c' && choice != 'p' && choice != 't' && choice != 'g')
+    {
+        cout << "Please enter a c, p, t, or g: ";
+        cin >> choice;
+    }
+    switch(choice)
+    {
+        case 'c' : cout << "carnivore choice.\n";
+                    break;
+        case 'p' : cout << "pianist choice.\n";
+                    break;
+        case 't' : cout << "A maple is a tree.\n";
+                    break;
+        case 'g' : cout << "game choice.\n";
+                    break;
+    }
+	return 0;
+}
