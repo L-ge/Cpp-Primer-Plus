1e1e22f6d8c29bd7c3e5e7c090b08afc8e61011e
L-ge <527773230@qq.com>
2017-08-04 22:54:17 +0800

    Add files via upload

---
+++ Chapter 6/Practice-6.8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+int main()
+{
+    using namespace std;
+    ifstream inFile;
+    cout << "Enter name of data file: ";
+    char filename[20];
+    cin.getline(filename, 20);
+    inFile.open(filename);
+    if (!inFile.is_open())
+    {
+        cout << "Could not open the file " << filename << endl;
+        cout << "Program terminating.\n";
+        exit(EXIT_FAILURE);
+    }
+    int count = 0;
+    char ch;
+    inFile >> ch;
+    while (inFile.good())
+    {
+        ++count;
+        inFile >> ch;
+    }
+    cout << "The number of character is " << count << endl;
+    inFile.close();
+	return 0;
+}
